8b583bb56d2df299d3b07a278b54f8184593c858
Ahmed Helal Ahmed <120987296+ahmed0helal0leetcode@users.noreply.github.com>
2023-02-21 19:05:54 +0200

    Time: 137 ms (36.42%), Space: 54.6 MB (29.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const lookup = {}
+    for (let num of nums) {
+
+        if (lookup[num] && lookup[num]) {
+            return true
+        } else {
+            lookup[num] = true;
+        }
+
+    }
+    
+    return false;
+
+};
